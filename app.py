4a818b532a3c27c291a82b9d61d64bde56bbcf22
CChandupa <chamodchandupa@gmail.com>
2026-04-01 14:48:17 +0530

    add loggers

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
 from flask import Flask, render_template, request, redirect
 from PredictionPipeline import preprocessing, vectorizer, get_prediction
+from logger import logging
 
 app = Flask(__name__)
+
+logging.info('Flask server started')
 
 data = dict()
 reviews = []
@@ -13,16 +16,27 @@ def index():
     data['reviews'] = reviews
     data['possitive'] = possitive
     data['negetive'] = negetive
+    
+    logging.info('************** Open home page **************')
+    
     return render_template('index.html', data=data)
+   
 
 @app.route("/", methods=['POST'])
 def my_post():
     global possitive, negetive 
-
     text = request.form['text']
+    
+    logging.info(f'Text : {text}')
+    
     preprocessed_text = preprocessing(text)
+    logging.info(f'Vectorized_Text : {preprocessed_text}')
+    
     vectorized_text = vectorizer(preprocessed_text)
+    logging.info(f'Vectorized_Text : {vectorized_text}')
+    
     prediction = get_prediction(vectorized_text)
+    logging.info(f'Prediction : {prediction}')
     
     if prediction == 'negetive':
         negetive += 1
